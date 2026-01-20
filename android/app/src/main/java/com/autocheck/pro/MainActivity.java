d6003a046c42930e393aa63192876c70a0f897fc
drsh3ban <dr.sh3ban@gmail.com>
2026-01-20 16:55:40 +0300

    Fix: Register CameraPlugin and FilesystemPlugin

--- android/app/src/main/java/com/autocheck/pro/MainActivity.java
+++ android/app/src/main/java/com/autocheck/pro/MainActivity.java
@@ -3,11 +3,15 @@ package com.autocheck.pro;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.capacitorjs.plugins.share.SharePlugin;
+import com.capacitorjs.plugins.camera.CameraPlugin;
+import com.capacitorjs.plugins.filesystem.FilesystemPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(SharePlugin.class);
+        registerPlugin(CameraPlugin.class);
+        registerPlugin(FilesystemPlugin.class);
     }
 }
